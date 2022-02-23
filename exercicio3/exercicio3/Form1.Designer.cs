e0f6e609ad645854501e154e2b4ceeec6786eadb
Jplmedinilha <88790228+Jplmedinilha@users.noreply.github.com>
2022-02-22 22:28:44 -0300

    Add files via upload

---
+++ exercicio3/exercicio3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace exercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtP1 = new System.Windows.Forms.TextBox();
+            this.txtP2 = new System.Windows.Forms.TextBox();
+            this.txtTCD = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(207, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(137, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtP1
+            // 
+            this.txtP1.Location = new System.Drawing.Point(207, 76);
+            this.txtP1.Name = "txtP1";
+            this.txtP1.Size = new System.Drawing.Size(137, 20);
+            this.txtP1.TabIndex = 1;
+            // 
+            // txtP2
+            // 
+            this.txtP2.Location = new System.Drawing.Point(207, 125);
+            this.txtP2.Name = "txtP2";
+            this.txtP2.Size = new System.Drawing.Size(137, 20);
+            this.txtP2.TabIndex = 2;
+            // 
+            // txtTCD
+            // 
+            this.txtTCD.Location = new System.Drawing.Point(207, 178);
+            this.txtTCD.Name = "txtTCD";
+            this.txtTCD.Size = new System.Drawing.Size(137, 20);
+            this.txtTCD.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nota 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota 2";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nota TCD";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(148, 245);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 297);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTCD);
+            this.Controls.Add(this.txtP2);
+            this.Controls.Add(this.txtP1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtP1;
+        private System.Windows.Forms.TextBox txtP2;
+        private System.Windows.Forms.TextBox txtTCD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ exercicio3/exercicio3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            decimal result = ((decimal.Parse(txtP1.Text) * 3 +
+                decimal.Parse(txtP2.Text) * 4 +
+                decimal.Parse(txtTCD.Text) * 3)) /10;
+
+            if (result >= 8)
+            {
+                MessageBox.Show("O resultado do aluno " + txtNome.Text + " foi MB, sendo aprovado com média " + result+".");
+            }
+            else if (result >= 6)
+            {
+                MessageBox.Show("O resultado do aluno " + txtNome.Text + " foi B, sendo aprovado com média " + result + ".");
+            }
+            else if (result >= 4)
+            {
+                MessageBox.Show("O resultado do aluno " + txtNome.Text + " foi R, sendo aprovado com média " + result + ".");
+            }
+            else if (result >= 0)
+            {
+                MessageBox.Show("O resultado do aluno " + txtNome.Text + " foi I, sendo aprovado com média " + result + ".");
+            } else
+            {
+                MessageBox.Show("Erro");
+            }
+
+
+        }
+    }
+}
